f2364d1398760f3285706f046329cdc812969982
sergio100899 <sergio100899@gmail.com>
2025-06-12 21:02:10 -0500

    New normal logic

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -43,3 +43,65 @@ def load_image_as_tensor_pil(path: str, grayscale: bool = False) -> torch.Tensor
         tensor = torch.from_numpy(img_np).permute(2, 0, 1)  # [3, H, W]
 
     return tensor
+
+
+def depth_to_normal(depth_map: torch.tensor, camera: torch.tensor):
+    depth_map = depth_map.squeeze()
+    height, width = depth_map.shape
+    points_world = torch.zeros((height + 1, width + 1, 3)).to(depth_map.device)
+    points_world[:height, :width, :] = unproject_depth_map(depth_map, camera)
+
+    p1 = points_world[:-1, :-1, :]
+    p2 = points_world[1:, :-1, :]
+    p3 = points_world[:-1, 1:, :]
+
+    v1 = p2 - p1
+    v2 = p3 - p1
+
+    normals = torch.cross(v1, v2, dim=-1)
+    normals = normals / (torch.norm(normals, dim=-1, keepdim=True) + 1e-8)
+
+    return normals
+
+
+def unproject_depth_map(depth_map: torch.tensor, camera: torch.tensor):
+    depth_map = depth_map.squeeze()
+    height, width = depth_map.shape
+    x = torch.linspace(0, width - 1, width).cuda()
+    y = torch.linspace(0, height - 1, height).cuda()
+    Y, X = torch.meshgrid(y, x, indexing="ij")
+
+    # Reshape the depth map and grid to N x 1
+    depth_flat = depth_map.reshape(-1)
+    X_flat = X.reshape(-1)
+    Y_flat = Y.reshape(-1)
+
+    # Normalize pixel coordinates to [-1, 1]
+    X_norm = (X_flat / (width - 1)) * 2 - 1
+    Y_norm = (Y_flat / (height - 1)) * 2 - 1
+
+    # Create homogeneous coordinates in the camera space
+    points_camera = torch.stack([X_norm, Y_norm, depth_flat], dim=-1)
+
+    K_matrix = camera.projection_matrix
+
+    # parse out f1, f2 from K_matrix
+    f1 = K_matrix[2, 2]
+    f2 = K_matrix[3, 2]
+
+    # get the scaled depth
+    sdepth = (f1 * points_camera[..., 2:3] + f2) / (points_camera[..., 2:3] + 1e-8)
+
+    # concatenate xy + scaled depth
+    points_camera = torch.cat((points_camera[..., 0:2], sdepth), dim=-1)
+    points_camera = points_camera.view((height, width, 3))
+    points_camera = torch.cat(
+        [points_camera, torch.ones_like(points_camera[:, :, :1])], dim=-1
+    )
+    points_world = torch.matmul(points_camera, camera.full_proj_transform.inverse())
+
+    # Discard the homogeneous coordinate
+    points_world = points_world[:, :, :3] / points_world[:, :, 3:]
+    points_world = points_world.view((height, width, 3))
+
+    return points_world
